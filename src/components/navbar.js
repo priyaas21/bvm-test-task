86db3bec48760dc0f705d359bb57a82b77bf784d
priyasingh21 <45866854+priyasingh21@users.noreply.github.com>
2023-04-14 15:39:17 +0530

    added navbar css

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,8 +1,16 @@
 import React from "react";
+import '../css/navbar.scss'
 
 const Navbar = () => {
     return (
-        <div style={{backgroundColor: 'blue'}}>Navbar</div>
+        <nav class="navbar">
+            <a href="#" class="navbar-brand">My Website</a>
+            <ul class="navbar-links">
+                <li><a href="#">Link 1</a></li>
+                <li><a href="#">Link 2</a></li>
+                <li><a href="#">Link 3</a></li>
+            </ul>
+        </nav>
     )
 }
 
