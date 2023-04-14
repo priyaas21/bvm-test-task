fba980590ff2fb734c851b9c5919d705e1a5fd9f
priyasingh21 <45866854+priyasingh21@users.noreply.github.com>
2023-04-14 15:27:46 +0530

    added footer css

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,8 +1,16 @@
 import React from "react";
+import '../css/footer.scss'
 
 const Footer = () => {
     return (
-        <div style={{backgroundColor: 'yellow'}}>Footer</div>
+        <footer class="footer">
+  <div class="footer-links">
+    <a href="#">Link 1</a>
+    {/* <a href="#">Link 2</a>
+    <a href="#">Link 3</a> */}
+  </div>
+  <p>&copy; 2023 My Website. All rights reserved.</p>
+</footer>
     )
 }
 
